cadeb15c467ef89d48aebf76e151260c1b3bb912
Aditya Mittal <adityamittal761@gmail.com>
2021-07-24 20:35:25 +0530

    image folder created

--- controllers/books/addbook.controller.php
+++ controllers/books/addbook.controller.php
@@ -12,7 +12,21 @@ if (isset($_POST['submit'])) {
 	$name=$_POST['name'];
 	$description=$_POST['description'];
 	$pdf=$_POST['pdf'];
-	$image=$_POST['cover_image'];
+	//$image=$_POST['cover_image'];
+	// to declare the things into here and later on we us these
+	$file = $_FILES['b_img'];
+
+	$fileName = $_FILES['b_img']['name'];
+	$fileTmpName = $_FILES['b_img']['tmp_name'];
+	$fileSize = $_FILES['b_img']['size'];
+	$fileError = $_FILES['b_img']['error'];
+	$fileType = $_FILES['b_img']['type'];
+
+	$fileExt = explode('.',$fileName);
+	$fileActualExt = strtolower(end($fileExt));
+
+	$allowed = array('jpg', 'jpeg', 'png');
+	 //ending of th decalration
 	$auth_name=$_POST['author_name'];
 	$total_count=$_POST['total_count'];
 
@@ -22,9 +36,25 @@ if (isset($_POST['submit'])) {
 		$_SESSION['msg'] =$nameError;
 	}
 
-	if (empty($_POST['cover_image'])) {
-		$imageError= "a cover image is required!";
-		$_SESSION['msg'] = $imageError;
+	// if (empty($_POST['cover_image'])) {
+	// 	$imageError= "a cover image is required!";
+	// 	$_SESSION['msg'] = $imageError;
+	// }
+	if(in_array($fileActualExt, $allowed)){
+		if($fileError === 0){
+			if($fileSize < 10000000){
+				$fileNameNew = uniqid('', true).".".$fileActualExt;
+				  $path = "resources\images/";
+				$fileDestination = $path.$fileNameNew;
+				move_uploaded_file($fileTmpName,$fileDestination);
+			}else{
+				echo "File to big";
+			}
+		}else{
+			echo "File doesn't uploaded";
+		}
+	}else{
+		echo "This file can't be uploaded";
 	}
 
 	
@@ -38,7 +68,7 @@ if (isset($_POST['submit'])) {
 		$_SESSION['msg'] = $countError;
 	}
 
-	if (empty($nameError) && empty($imageError) && empty($authorError) && empty($countError)) {
+	if (empty($nameError)  && empty($authorError) && empty($countError)) {
 		//SQL query to insert the data
         App::get('books')->addBook($name,$auth_name,$description,$image,$pdf,$total_count);
 		// require './view/addbook.php';
--- view/addbook.php
+++ view/addbook.php
@@ -1,54 +1,3 @@
-<?php
-
-//including database connection file
-//include_once 'connect.php';
-
-// $cover_image = $name = $author_name = $total_count = " ";
-// $_SESSION = array('name' => '', 'author_name' => '', 'cover_image' => '', 'total_count' => '');
-
-// if (isset($_POST['submit'])) {
-
-// 	//checking the errors (i.e., checking whether the field is empty or not)
-// 	if (empty($_POST['name'])) {
-// 		$errors['name'] = "A name is required! ";
-// 	}
-
-// 	if (empty($_POST['cover_image'])) {
-// 		$errors['cover_image'] = 'a cover image is required! <br />';
-
-// 	}
-// 	if (empty($_POST['author_name'])) {
-// 		$errors['author_name'] = "An author name is required!";
-// 	}
-// 	if (empty($_POST['total_count'])) {
-// 		$errors['total_count'] = 'Please enter the count of the books!';
-// 	}
-
-// 	if (array_filter($errors)) {
-// 		echo 'something is missing';
-// 	}
-
-// 	//SQL query to insert the data
-// 	else {
-// 		$sql = "INSERT INTO books (name,author_name,description,cover_image , pdf,total_count) VALUES('" . $_POST["name"] . "','" . $_POST["author_name"] . "','" . $_POST["description"] . "','" . $_POST["cover_image"] . "','" . $_POST["pdf"] . "','" . $_POST["total_count"] . "')";
-
-// 		$result = mysqli_query($conn, $sql);
-//
- // 		if (result) {
-// 			<script type="text/javascript">
-// 					alert("Book Added Successfully!");
-// 					window.location.href="admin_booklist.php";
-// 			</script>
-// 			} -->
-// 	<?php
-// }
-
-// }
-
-// mysqli_close($conn);
-
-?>
-
 <!DOCTYPE html>
 <html>
  <head>
@@ -59,7 +8,7 @@
  	<section class="container black-text">
  		<h4 class="center black-text">Add Book</h4>
 		 <div class="red-text center"><?php echo $_SESSION['msg']; ?></div> 
-	 <form action="/addbook" method="POST">
+	 <form action="/addbook" method="POST" enctype="multipart/form-data">
 		<label>Name *:</label>
 		<br>
 		<input type="text" name="name" required>
@@ -74,7 +23,7 @@
 		<br><br>
 		<label type="cover_image">Image *:</label>
 		<br>
-		<input type="url" name="cover_image" required>
+		<input type="file" name="b_img" id="b_img" value="Upload" required>
 		<br><br>
 		<label>pdf:</label>
 		<br>
